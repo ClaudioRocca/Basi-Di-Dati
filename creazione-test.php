06b18acbf3ada5831e0f11a30f7ec56031018f89
ClaudioRocca <claudiorocca02@gmail.com>
2024-03-05 14:00:07 +0100

    Sviluppo creazione quesiti

--- creazione-test.php
+++ creazione-test.php
@@ -7,3 +7,32 @@
     }
 
 ?>
+
+
+<!DOCTYPE html>
+<html lang="it">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Crea Tabelle di Esercizio</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+
+<body>
+    <div class="container mt-5">
+        <h2>Crea Nuovo test</h2>
+        <form action="creazione-quesito.php" method="post">
+            <div class="form-group">
+                <label for="nomeTest">Nome Test</label>
+                <input type="text" class="form-control" id="nomeTest" name="nomeTest" required>
+            </div>
+            <button type="submit" class="btn btn-primary">Crea Test</button>
+        </form>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+
+</html>
