18d6efb05372fe3caaf2cc7303844312a933221a
ClaudioRocca <claudiorocca02@gmail.com>
2024-03-06 19:23:29 +0100

    Cambio titolo dashboard studente

--- interfaccia-studente.php
+++ interfaccia-studente.php
@@ -14,7 +14,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard Docente</title>
+    <title>Dashboard Studente</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 
