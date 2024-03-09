10e0157b38f73b7d5487896db9270b6f01659002
ClaudioRocca <claudiorocca02@gmail.com>
2024-03-09 12:38:14 +0100

    Creazione quesiti da parte del docente

--- interfaccia-docente.php
+++ interfaccia-docente.php
@@ -25,7 +25,6 @@
             <a href="creazione-tabelle.php" class="list-group-item list-group-item-action">Crea Tabella di Esercizio</a>
             <a href="visualizzazione-test.php" class="list-group-item list-group-item-action">Visualizza test</a>
             <a href="creazione-test.php" class="list-group-item list-group-item-action">Crea nuovo Test</a>
-<!--            <a href="creazione-quesito.php" class="list-group-item list-group-item-action">Crea nuovo quesito</a>-->
             <a href="invio-messaggi-docente.php" class="list-group-item list-group-item-action">Invia messaggio</a>
             <a href="statistiche.php" class="list-group-item list-group-item-action">Visualizzazione Statistiche</a>
             <a href="log-eventi.php" class="list-group-item list-group-item-action">Amministrazione Log Eventi</a>
